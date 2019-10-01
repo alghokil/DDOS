cbe476a22ea73c28ac8dad4c40ef7ecce30f1d93
alghokil <54709070+alghokil@users.noreply.github.com>
2019-10-01 20:25:26 +0700

    Add files via upload

---
+++ Toolsx.sh
@@ -0,0 +1,80 @@
+GNU nano 4.4                  Tools-Attack.sh
+#!/bin/bash
+#Variables
+b="\033[1m"
+u="\033[4m"
+bl="\033[30m"
+r="\033[31m"
+g="\033[32m"
+bu="\033[34m"
+m="\033[35m"
+c="\033[36m"
+w="\033[37m"
+endc="\033[0m"
+enda="\033[0m"
+blue="\033[1;34m"
+cyan="\033[1;36m"
+red="\033[1;31m"
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet DDOS-TOOLS | lolcat
+echo         "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo         " Creadby   : Al Ghokil $white                      " | lolcat
+echo         " Team        : Cyber Muslim Uknnown Friend $white  " | lolcat
+echo         " Contact     : 0813×××××××××××××××× $white         " | lolcat
+echo         " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+
+echo ""
+
+echo "1.> hammer"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo "2.> lucita-ddos"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo "3.> Litedos"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo "4.> Exit"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo ""
+read -p "root@T00Ls-alghokil~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/cyweb/hammer
+mv hammer $HOME
+cd $HOME/hammer
+python hammer.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim" | lolcat
+git clone https://github.com/zlucifer/lucita_ddos
+mv lucita_ddos $HOME
+cd $HOME/lucita_ddos
+python licita_ddos.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo GUNAKAN ILMU KALIAN DENGAN BIJAK | lolcat -a -d 50
+clear
+figlet "cybermuslim"
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+echo "created by : AlGhokil" | lolcat
+exit
